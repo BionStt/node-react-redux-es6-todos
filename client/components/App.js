18b8f52be41a507b67c60629ccbd659736c70b2d
Ibro <ibro@ibro.me>
2016-04-01 08:08:05 +0200

    Adding simple app component

---
+++ client/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+    <div>
+        Todos app
+    </div>
+);
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/App';
+var reducers = function(){};
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App></App>
+    </Provider>,
+    document.getElementById('mainContainer')
+);
