4a2867df699e5a8de3bf47d4a2eaa451e8bce480
Ibro <ibro@ibro.me>
2016-04-02 07:39:53 +0200

    Cleanup

--- client/reducers/todos_reducer.js
+++ client/reducers/todos_reducer.js
@@ -1,26 +1,38 @@
 import * as constants from '../constants';
 
-function todoApp(state = [], action) {
+function todo(state, action) {
     switch (action.type) {
         case constants.ADD_TODO:
-            var newTodo = {
+            return {
                 id: action.id,
                 text: action.text,
                 completed: false
             };
-            return [...state, newTodo];
         case constants.TOGGLE_TODO:
-            return state.map((todo, index) => {
-                if (index === action.id) {
-                    return Object.assign({}, todo, {
-                        completed: true
-                    });
-                }
-                return todo;
+            if (state.id !== action.id) {
+                return state;
+            }
+            return Object.assign({}, state, {
+                completed: !state.completed
             });
+
         default:
             return state;
     }
 }
 
-export default todoApp;
+function todos(state = [], action) {
+    switch (action.type) {
+        case constants.ADD_TODO:
+            var newTodo = todo(undefined, action);
+            return [...state, newTodo];
+        case constants.TOGGLE_TODO:
+            return state.map(t =>
+                todo(t, action)
+            );
+        default:
+            return state;
+    }
+};
+
+export default todos
--- client/reducers/visibilityFilter_reducer.js
+++ client/reducers/visibilityFilter_reducer.js
@@ -1,7 +1,7 @@
 import * as filterConstants from '../constants/filters';
 
 
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
+const visibilityFilter = (state = filterConstants.SHOW_ALL, action) => {
     switch (action.type) {
         case filterConstants.SET_VISIBILITY_FILTER:
             return action.filter;
